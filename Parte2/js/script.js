24211e41ca284ca1c650b15c980410f8ddb9f63a
Alexis Zugasti <alexiszugasti@gmail.com>
2022-07-11 18:32:23 -0500

    Added 1.4 javascript exercises

--- Parte2/js/script.js
+++ Parte2/js/script.js
@@ -11,8 +11,16 @@
         4) Escribe en el textArea los nuevos valores
 
         Realiza todo lo anterior al hacer click en el botón. 
-        */
-
+      */
+      let nameButton = document.getElementById("btn_mayusculas")      //Se Obtiene la referencia del boton
+      nameButton.addEventListener("click", postNames)                 //Se agrega un EventListener en click
+      
+      function postNames(e){                                          //Se pasa el evento como parametro
+        e.preventDefault()                                            //Se evita que la pagina recargue al omprimir el boton
+        let names = document.getElementById("txt_may").value          //Se obtienen los nombres como string
+        let newNames = names.toLowerCase();                           //Se transforman los valores a minusculas
+        document.getElementById("txt_may").value = newNames           //Se escriben nuevamente los valores en el textarea
+      }
 
       /*
         Crea una funcion llamada "calculateDogAge" que: 
@@ -25,10 +33,17 @@
            Deberás convertir el string a Entero. 
     
     */
+      let convertButton = document.getElementById("btn_añoshumano")         //Se obtiene la referencia del boton
+      convertButton.addEventListener("click", convert)                      //Se agrega el eventlistener
+      function convert(e){        
+        e.preventDefault()                                                  //Se evita que se recargue la pagina
+        let value = parseInt(document.getElementById("edad_humano").value)  //Se obtiene el valor
+        let newValue = value * 7;                                           //Se moficia el valor
+        document.getElementById("edad_perro").value = newValue              //Se escrib ele valor en el campo correspondiente
+      }
 
 
-
-      /*  OBJETOS 
+    /*  OBJETOS 
     
     A) Crea un constructor de tipo "Pokemon" que reciba 4 parametros
     
@@ -45,3 +60,30 @@
        Registra un evento de "click", para que cuando se de click en el botón, se muestren las propiedades de cada objeto en el input correspondiente. 
 
      */
+
+    function pokemon(name, species, attack, defense){                           //Se crea el constructor del objeto
+      this.name = name
+      this.species = species
+      this.attack= attack
+      this.defense = defense
+    }
+
+    let pokemon1 = new pokemon("Bulbasaur","Grass",49,50)                       //Se crean ambos objetos
+    let pokemon2 = new pokemon("Charmander", "Fire",52,43)
+
+    let pokeButton = document.getElementById("btn_muestra")                     //Se obtiene la referencia del boton
+    pokeButton.addEventListener("click", showPokemon)                           //Se agrega un eventlistener
+
+    function showPokemon(e){
+      e.preventDefault()                                                        //Se evita que la pagina se recargue al oprimir el boton
+
+      document.getElementById("nombre_poke1").value = pokemon1.name             //Se escribe la informacion del primer pokemon
+      document.getElementById("especie_poke1").value = pokemon1.species
+      document.getElementById("ataque_poke1").value = pokemon1.attack
+      document.getElementById("defensa_poke1").value = pokemon1.defense
+
+      document.getElementById("nombre_poke2").value = pokemon2.name             //Se escribe la informacion del segundo pokemon
+      document.getElementById("especie_poke2").value = pokemon2.species
+      document.getElementById("ataque_poke2").value = pokemon2.attack
+      document.getElementById("defensa_poke2").value = pokemon2.defense
+    }
